86f76ee3d6467c4367f3dead4640ce510161a02d
Ilya <pistol88@gmail.com>
2017-03-21 01:03:24 +0600

    OrderManager

--- src/interfaces/data/OrderManager.php
+++ src/interfaces/data/OrderManager.php
@@ -1,1 +1,20 @@
 
+<?php
+namespace dvizh\app\interfaces\services;
+
+interface OrderManager
+{
+    public function getById(int $id) : \dvizh\app\interfaces\entities\Order;
+  
+    public function getUserTotalSum(\dvizh\app\interfaces\entities\User $user) : int;
+  
+    public function filling();
+  
+    public function cancel();
+  
+    public function cancelElement(\dvizh\app\interfaces\entities\CartElement $element);
+  
+    public function putElement(\dvizh\app\interfaces\entities\CartElement $element) : int;
+  
+    public function setStatus($status);
+}
